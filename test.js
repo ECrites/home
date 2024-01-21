3f56ef1ca8cda3ea5c978a237fd12f295647e0de
Ethan Crites <ethan.crites@southeasttech.edu>
2024-01-21 15:44:17 -0600

    Added test.js

---
+++ test.js
@@ -0,0 +1,18 @@
+
+
+let testVar = prompt("Give me how many numbers you want")
+
+for (let i = 0; i <= 5; i++) {
+
+    if (testVar % 2 == 00) {
+        testVar++;
+    }
+    else
+    {
+        console.log(testVar);
+        testVar++;
+    }
+
+
+}
+
