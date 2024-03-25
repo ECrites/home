53c7ff4dbb3c5d4295cc7abc8bc21301d42c6bb2
Ethan Crites <ethan.crites@southeasttech.edu>
2024-03-24 20:19:22 -0500

    bugfix 4

--- unit2/unittwo.js
+++ unit2/unittwo.js
@@ -1,7 +1,7 @@
 import{HP} from './lib_objects.js'
 const playerHp = new HP(10);
 HP.getHp();
-function drinkPotion(){
+function drinkPotion() {
 	if (Math.floor(Math.random() * 1) == 0){
 		HP.takeHp();
 		HP.getHp();
